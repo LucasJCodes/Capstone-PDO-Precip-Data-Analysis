aa18ef6784337a434defd081d6cf5c5bfb39e557
LucasJCodes <lucas.jones.home@gmail.com>
2024-10-24 13:23:04 -0500

    minor update to make it a main method instead of a function:

--- Phase Change Scripts/mainMethod_phaseChange.py
+++ Phase Change Scripts/mainMethod_phaseChange.py
@@ -12,15 +12,15 @@ import performEOF
 import PDOindex
 
 # The first input is the subsetted SST netCDF data from Roy's computer.
-def phaseChange(subsetted_SST):
-    # The performEOF function has input (ssts) and output (pc1), an xarray dataset with the first principle component of inputted SSTs, or the PDO index.
-    pc1 = performEOF.performEOF(subsetted_SST)
 
-    # The ID_Phase function has inputs (data, period, bound) and output (neutral_dates), a list of months that have been identified as PDO neutral using the rolling average. 
-        #data is PDO index values by month.
-        #We want the output of performEOF, pc1, to be the data input for ID_phase. So ID_phase must be able to import an xarray.
+# The performEOF function has input (ssts) and output (pc1), an xarray dataset with the first principle component of inputted SSTs, or the PDO index.
+pc1 = performEOF.performEOF(subsetted_SST)
+
+# The ID_Phase function has inputs (data, period, bound) and output (neutral_dates), a list of months that have been identified as PDO neutral using the rolling average. 
+    #data is PDO index values by month.
+    #We want the output of performEOF, pc1, to be the data input for ID_phase. So ID_phase must be able to import an xarray.
     
-    neutral = ID_Phase(pc1, period = 36, bound = 0.5)
+neutral = PDOindex.ID_Phase(pc1, period = 36, bound = 0.5)
 
 """
 Note: This function will not work as written because pc1 is an xarray and ID_Phase takes in a CSV file. Need to make performEOF output compatible with ID_Phase, or vice versa.
