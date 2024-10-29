66220d3faa9bb7f252d8eccd594bc95a2aedc220
deforebri <defore.bri@gmail.com>
2024-10-28 21:14:51 -0500

    Additional figure for determining PDO phase change

--- Phase Change Scripts/PDOindex.py
+++ Phase Change Scripts/PDOindex.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime
 import pandas as pd
+import xarray as xr
 
-def ID_Phase(data, period, bound):
+def ID_Phase(PDOindex, period, bound):
     
-    PDOindex = pd.DataFrame(data)
-
     PDOindex['Date'] = pd.to_datetime(PDOindex[['Year', 'Month']].assign(Day=1))
 
     index = PDOindex["Value"]
 
     Neutral = False
     NeutralDates = []
+    PhaseChange = []
 
     for i in range(0, len(PDOindex)-period):
         SUM = 0
@@ -26,14 +26,32 @@ def ID_Phase(data, period, bound):
         if ((Average >= -bound) and (Average <= bound)):
             Neutral = True
             NeutralDates.append(PDOindex['Date'][i])
+            PhaseChange.append('1')
 
+        else:
+            PhaseChange.append('0')
+    
+    for i in range(0, period):
+        PhaseChange.append('0')
+    
+    PDOindex['Phase Change'] = PhaseChange
+
+    #print(PDOindex)
 
     print(len(NeutralDates))
 
     PDOindex['Color'] = ['red' if value > bound else 'blue' if value < -bound else 'black' for value in PDOindex['Value']]
 
-    fig,ax = plt.subplots(figsize=(16,4))
+
+    fig,ax = plt.subplots(figsize=(15,4))
     ax.bar(PDOindex['Date'], PDOindex['Value'], width=45, color=PDOindex['Color'])
+    plt.fill_between(PDOindex['Date'], PDOindex['Value'], 4, where=(PDOindex['Value'] >= -bound), color='lightblue', alpha=0.3, step='mid')
+    plt.fill_between(PDOindex['Date'], PDOindex['Value'], 4, where=(PDOindex['Value'] <= bound), color='lightblue', alpha=0.3, step='mid')
+    #plt.show()
+
+    plt.figure(figsize=(15,4))
+    plt.step(PDOindex['Date'], PDOindex['Phase Change'])
+    plt.ylim(0,2)
     plt.show()
 
     return NeutralDates
@@ -43,5 +61,7 @@ def ID_Phase(data, period, bound):
 file = "NOAA_PDO_Index.csv"
 
 NOAAdata = pd.read_csv(file)
-neutral = ID_Phase(NOAAdata, 36, 0.5)
+PDOindex = pd.DataFrame(NOAAdata)
+
+neutral = ID_Phase(PDOindex, 72, 0.1)
 #print(NOAAdata)
