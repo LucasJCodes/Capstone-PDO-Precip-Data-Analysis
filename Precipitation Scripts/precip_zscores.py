c4ea7117af11ca45f7c3470ce2d4aaad2435b5e9
Daniel Fenske <danielfenske76@gmail.com>
2024-11-04 19:38:40 -0600

    adding to zscores

--- Precipitation Scripts/precip_zscores.py
+++ Precipitation Scripts/precip_zscores.py
@@ -4,11 +4,17 @@ import xarray as xr
 import numpy as np
 
 # Step 1: Average precip for each month across each member's ~100 years of data.
-def member_pcp(dataset):
+def member_zscore(dataset):
     # Input 1 will be monthly precip data from one CESM2 member, which has dimensions of lat, lon, and time. Time component will be in months.
 
+    pcp = dataset.groupby('time.month')
     avgs = dataset.groupby('time.month').mean(dim = 'time')
+    stds = dataset.groupby('time.month').std(dim = 'time')
+    zscores = (pcp - avgs) / stds
+
+    return pcp
     return avgs
+    return zscores
 
 
 
