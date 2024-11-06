c7e48550013f697badfc249a570af0eb8261bd55
Daniel Fenske <danielfenske76@gmail.com>
2024-11-06 10:47:11 -0600

    work from yesterday on zscores

--- Precipitation Scripts/precip_zscores.py
+++ Precipitation Scripts/precip_zscores.py
@@ -3,6 +3,7 @@
 import xarray as xr
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Step 1: Average precip for each month across each member's ~100 years of data.
 def member_zscore(filepath):
@@ -23,9 +24,22 @@ def member_zscore(filepath):
 
     # Z-scores calculation
     zscores = (pcp - avgs) / stds
+
+    # Now, to check things are going ok, let's plot the zscores on a contour plot. Yay.
+
     return zscores
     
-mem12 = member_zscore('/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Data/PRECTmem11.nc')
+data = member_zscore('/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Data/PRECTmem11.nc')
 
-#print(mem12)
 
+print(data.time)
+data.time.dt.strftime("%Y-%m-%d %H:%M:%S")
+data = data.where(data.time.dt.strftime("%Y-%m-%d %H:%M:%S").year == 1950, drop = True)
+# print(data)
+print(data.time.shape)
+
+
+# print(data_slice)
+
+# plt.contourf(data.lat, data.lon, data_slice)
+# plt.show()
