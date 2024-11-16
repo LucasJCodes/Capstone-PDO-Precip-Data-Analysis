c0ec829d7c364a7d50e172330c25319cb42c7b41
rpdpg4 <rpdgalang@gmail.com>
2024-11-16 14:35:43 -0600

    Added extra comments to the code (still WIP)

--- Precipitation Scripts/zscores_average.py
+++ Precipitation Scripts/zscores_average.py
@@ -6,7 +6,10 @@ import numpy as np
 def phaseMask(filepaths, pc_months):
     # Zscores is the zscores dataset and pc_months is the list of phase change months.
 
-    zscores = xr.open_mfdataset(filepaths)
+    #Read in the dataset
+    zscores = xr.open_mfdataset(filepaths, engine="netcdf4")
+    #Take the zscores from 'precip_zscores.py', then subset those based on the months that ARE phase change months
+        #In other words, if the month of a zscore is a phase change month, keep. Otherwise, drop
     phaseChange_zs = zscores.where(zscores['time.month'].isin(pc_months), drop=True)
 
     # Calculate the average zscores for each month at each gridpoint during phase changes.
