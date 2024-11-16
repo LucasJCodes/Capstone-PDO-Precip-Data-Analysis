a347d9620d7ec194a14deece677b1db2f226226d
Daniel Fenske <danielfenske76@gmail.com>
2024-11-16 15:55:17 -0600

    found avg annual pcp all members

--- Precipitation Scripts/precip_monthlyAvgs.py
+++ Precipitation Scripts/precip_monthlyAvgs.py
@@ -9,28 +9,35 @@ import numpy as np
 def pcp_monthlyAvg(filepath):
     # Input will be the filepath to a netCDF of monthly precip data from one CESM2 member, which has dimensions of lat, lon, and time. Time component will be in months.
     
-    dataset = xr.open_dataset(filepath)
+    ds = xr.open_mfdataset(filepath, combine = 'nested', concat_dim = 'member')
 
     # Grab only the precip portion of the dataset.
-    prect = dataset['PRECT']
+    prect = ds['PRECT']
+
     # Convert units from m/s to mm/month.
     prect = prect * 3600 * 24 * 30.417 * 1000
    
-    # Precip data grouped by month
+    # # Precip data grouped by month
     pcp = prect.groupby('time.month')
+
     # Average for each month's precip (e.g. all Januarys)
-    avgs = pcp.mean(dim = 'time')
-    return avgs
+    mem_sum = pcp.mean(dim = ['member', 'time'])
+    avg = mem_sum.sum(dim = 'month')
+    #avg = mem_sum.mean(dim = 'month')
 
-test = pcp_monthlyAvg(filepath = '/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Data/PRECTmem11.nc')
+    #annual_sum = pcp.sum(dim = 'member').groupby('time.month')
+    #annual_avg = annual_sum.mean(dim = 'month')
+    return avg
 
-jun = test.where(test.month == 8, drop = True)
-jun = jun.squeeze(('month'), drop = True)
+test = pcp_monthlyAvg(filepath = '/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Data/PRECTmem*.nc')
 
-X, Y = np.meshgrid(jun.lon, jun.lat)
+# tot = test.where(test.month == 8, drop = True)
+# tot = jun.squeeze(('month'), drop = True)
+
+X, Y = np.meshgrid(test.lon, test.lat)
 fig, ax = plt.subplots(nrows = 1, ncols = 1, subplot_kw = {'projection': ccrs.PlateCarree()})
 ax.coastlines()
-my_ax = ax.contourf(X, Y, jun, transform = ccrs.PlateCarree())
+my_ax = ax.contourf(X, Y, test, transform = ccrs.PlateCarree())
 ax.add_feature(cfeature.STATES, zorder=1, linewidth=1, edgecolor='k')
 fig.colorbar(my_ax, ax = ax)
-plt.show()
+plt.savefig('avgAnnual_allMembers.png')
