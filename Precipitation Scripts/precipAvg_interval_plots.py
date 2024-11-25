be9a51b6684e39a2e78bdb24d1e591d8629427a2
Daniel Fenske <danielfenske76@gmail.com>
2024-11-24 21:53:16 -0600

    avg precip non-phase changes

--- Precipitation Scripts/precipAvg_interval_plots.py
+++ Precipitation Scripts/precipAvg_interval_plots.py
@@ -7,10 +7,23 @@ import precipAvg_interval as pcp
 filepaths = '/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Data/PRECTmem*.nc'
 data = pcp.pcpAvg(filepaths)
 
-# Avg Summertime Precip
-summertime = pcp.interval(data, months = np.arange(6, 9))
-pcp.prettyColors(data = roy, title = 'Average Summertime Precipitation (mm)', cmap = 'YlGn', save = 'avgSummer_precip.png')
+# Make seasons to reference in interval function.
+winter = np.arange(0, 3)
+spring = np.arange(3, 6)
+summer = np.arange(6, 9)
+fall = np.arange(9, 12)
+seasons = [winter, spring, summer, fall]
+
+# Run interval function to get averages by season.
+seasonal_avgs = []
+for i in seasons:
+    seasonal_avgs.append(pcp.interval(data, i))
+
+#Plot the selected month with prettyColors function.
+szn_names = ['Winter', 'Spring', 'Summer', 'Fall']
+for i in range(0, 4):
+    pcp.prettyColors(seasonal_avgs[i], title = 'Average ' + szn_names[i] + ' Precipitation (mm)', cmap = 'YlGn', save = '/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Plots/' + szn_names[i] + 'AvgPrecip.png')
 
 # Avg Annual Precip
 annual = pcp.interval(data, months = np.arange(1, 13))
-pcp.prettyColors(data = daniel, title = 'Average Annual Precipitation (mm)', cmap = 'YlGn', save = 'avgAnnual_precip.png')
+pcp.prettyColors(annual, title = 'Average Annual Precipitation (mm)', cmap = 'YlGn', save = '/Users/dfencekey/Desktop/Coding/Capstone/Capstone-PDO-Precip-Data-Analysis/Plots/AnnualAvgPrecip.png')
