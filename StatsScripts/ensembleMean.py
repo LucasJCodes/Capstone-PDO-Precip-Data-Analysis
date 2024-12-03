4ce27f3b48c7af5f10fc9da88f6e73cc7b225d6f
LucasJCodes <lucas.jones.home@gmail.com>
2024-12-02 20:44:20 -0600

    Added t test script and changed folder name

---
+++ StatsScripts/ensembleMean.py
@@ -0,0 +1,35 @@
+import xarray as xr
+
+def ensMean(data, time):
+    """
+    This function computes the mean of precipitation data from a multi-member ensemble of the CESM2-LE experiment.
+
+    Authors: 
+        Lucas Jones in collaboration with Brianna DeFore, Daniel Fenske, Roy Galang
+
+    Args:
+        data- the xarray datasets to perform the calculation on.
+        
+
+    Returns:
+        An xarray dataset that holds the ensemble mean of precipitation data for 10 members
+        
+    """
+
+    #calculate the mean along the members and by time
+    byTime = data.groupby("time.month")
+    ensAvg = data.mean(dim = ["member", "time"])
+
+    #return the dataset 
+    return ensAvg
+
+##############
+
+"""
+filenames = "Data/PRECTmem*.nc"
+
+avgs = ensMean(filenames)
+"""
+
+test = xr.open_dataset("Data/PRECTmem11.nc")
+print(test)
---
+++ StatsScripts/ttest.py
@@ -0,0 +1,44 @@
+#This method performs a t test on the two given gridded data sets and returns an xarray holding
+#gridded pvalues for locations that exceed the significance threshold, alpha.
+#The t test is two sidded, performed along the first axis, and assumes equal variance
+
+import numpy as np
+from scipy import stats
+import xarray as xr
+
+def ttest(data1, data2, alpha):
+    """
+        This function performs a t test on  two given gridded data sets and returns an xarray dataset 
+        holding gridded pvalues for locations that exceed the significance threshold, alpha.
+        The t test is two sidded, performed along the first axis, and assumes equal variance
+
+        Authors: 
+            Lucas Jones in collaboration with Brianna DeFore, Daniel Fenske, Roy Galang
+
+        Args:
+            data1- the first xarray dataset to perform the t test on.
+            data2- the second xarray dataset to perform the t test on.
+            alpha- the significance threshold by which the null hypothesis is tested.
+
+        Returns:
+            An array of the p-values which are less than the alpha significance threshold.
+        
+    """
+
+    #perform the t test
+    tstat, pval = stats.ttest_ind(data1, data2, axis = 0, equal_var = True, alternative = "two-sided")
+    
+    #create latitude and longitude arrays for the plotting the p values based on our western US region
+    lat = np.arange(25.92, 55, 0.94)  
+    lon = np.arange(225, 260, 1.25)  
+
+    #create a data array holding the p values and add a cyclic point to remove blank line
+    xpval = xr.DataArray(pval, coords = {"latitude": lat, "longitude": lon}, dims = ["latitude", "longitude"])
+
+    #get only statistically significant p values for plotting
+    
+    print(xpval.min())
+
+    sig_pval = xpval.where(xpval.values <= alpha)
+
+    return sig_pval
